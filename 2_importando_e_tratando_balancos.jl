15f82d1c16ff1e28de9cfaa4c225c0ad775f06ff
rtheodoro <rtheodoro@usp.br>
2023-09-14 01:41:45 -0300

    falta apenas trocar as virgulas por pontos

--- 2_importando_e_tratando_balancos.jl
+++ 2_importando_e_tratando_balancos.jl
@@ -15,33 +15,40 @@ for i in primeiroano:anomaisrecente
         csv_i = CSV.File("data_raw/$(i)12COOPERATIVAS.CSV", delim=';', header=5) |> DataFrame        
         println("Tratando Balanço do ano $i")
         csv_i = filter(row -> row.DOCUMENTO == 4010, csv_i)
-        csv_i = select(csv_i, :CNPJ, Symbol("#DATA_BASE"), :NOME_INSTITUICAO, :CONTA, :SALDO)
+        csv_i = select(csv_i, :CNPJ, Symbol("#DATA_BASE") => :DATA, :NOME_INSTITUICAO, :CONTA, :SALDO)
     elseif i > 1993 && i < 2010
         csv_i = CSV.File("data_raw/$(i)12COOPERATIVAS.CSV", delim=';', header=4) |> DataFrame 
         println("Tratando Balanço do ano $i")
         csv_i = filter(row -> row.DOCUMENTO == 4010, csv_i)
-        csv_i = select(csv_i, :CNPJ, :DATA, Symbol("NOME INSTITUICAO"), :CONTA, Symbol("SALDO"))
+        csv_i = select(csv_i, :CNPJ, :DATA, Symbol("NOME INSTITUICAO") => :NOME_INSTITUICAO, :CONTA, 8 => :SALDO)
     else
         csv_i = CSV.File("data_raw/$(i)12COOPERATIVAS.CSV", delim=';', header=4) |> DataFrame 
         println("Tratando Balanço do ano $i")
         csv_i = filter(row -> row.DOCUMENTO == 4010, csv_i)
-        csv_i = select(csv_i, :CNPJ, Symbol("#DATA_BASE"), :NOME_INSTITUICAO, :CONTA, :SALDO)
+        csv_i = select(csv_i, :CNPJ, Symbol("#DATA_BASE") => :DATA, :NOME_INSTITUICAO, :CONTA, :SALDO)
     end
 
-    rename!(csv_i, [Symbol("CNPJ") => :cnpj, Symbol("#DATA_BASE") => :ano, Symbol("NOME_INSTITUICAO") => :razao_social])
+    rename!(csv_i, [Symbol("CNPJ") => :cnpj, Symbol("DATA") => :ano, Symbol("NOME_INSTITUICAO") => :razao_social])
     
     csv_i = unstack(csv_i, :CONTA, :SALDO)
 
     println("Unificando Balanço do ano $i")
-    csv_coop_completo_1993a2022 = vcat(csv_coop_completo_1993a2022, csv_i)
+    
+    csv_coop_completo_1993a2022 = vcat(csv_coop_completo_1993a2022, csv_i, cols=:union)
     empty!(csv_i)
 end
 
-# Substitua 'csv_coop_completo_1993a2022' pelo nome real do seu data frame
-for col in names(csv_coop_completo_1993a2022)[4:end]
-    csv_coop_completo_1993a2022[!, col] = replace.(csv_coop_completo_1993a2022[!, col], r"," => ".")
-    csv_coop_completo_1993a2022[!, col] = parse.(Float64, csv_coop_completo_1993a2022[!, col])
-end
+# Substituindo virgulas por pontos
+#for col in names(csv_coop_completo_1993a2022)[4:end]
+    # Substitua todas as vírgulas por pontos
+#    csv_coop_completo_1993a2022[!, col] = replace.(string.(csv_coop_completo_1993a2022[!, col]), "," => ".")
+    
+    # Substitua "missing" por 0.0 (ou outro valor padrão)
+#    csv_coop_completo_1993a2022[!, col] .= coalesce.(csv_coop_completo_1993a2022[!, col], 0.0)
+    
+    # Converta para Float64
+#    csv_coop_completo_1993a2022[!, col] = parse.(Float64, csv_coop_completo_1993a2022[!, col])
+#end
 
 CSV.write("data/balanco_coop_cred_1993a2022_4010.csv", csv_coop_completo_1993a2022, writeheader=true)
 
